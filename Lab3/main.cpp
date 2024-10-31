0057086c431b70edf96965fcfe563d2530350adf
tavim26 <tavimirisan26@gmail.com>
2024-10-31 10:48:13 +0200

    commit

---
+++ Lab3/main.cpp
@@ -0,0 +1,266 @@
+//
+//  main.cpp
+//  OpenGL_Shader_Example_step1
+//
+//  Created by CGIS on 30/11/15.
+//  Copyright © 2015 CGIS. All rights reserved.
+//
+
+#if defined (__APPLE__)
+    #define GLFW_INCLUDE_GLCOREARB
+    #define GL_SILENCE_DEPRECATION
+#else
+    #define GLEW_STATIC
+    #include <GL/glew.h>
+#endif
+
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+int glWindowWidth = 640;
+int glWindowHeight = 480;
+int retina_width, retina_height;
+GLFWwindow* glWindow = NULL;
+
+GLuint shaderProgram;
+GLuint shaderProgramGreen;
+
+GLfloat firstTriangle[] = {
+    -0.5f,  0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+     0.5f,  0.5f, 0.0f
+};
+
+GLfloat secondTriangle[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.5f,  0.5f, 0.0f
+};
+
+GLuint VBO1, VAO1;
+GLuint VBO2, VAO2;
+
+GLuint verticesVBO;
+GLuint triangleVAO;
+
+void windowResizeCallback(GLFWwindow* window, int width, int height)
+{
+    fprintf(stdout, "window resized to width: %d , and height: %d\n", width, height);
+    //TODO
+}
+
+void initObjects()
+{
+    // Primul triunghi
+    glGenBuffers(1, &VBO1);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+    glGenVertexArrays(1, &VAO1);
+    glBindVertexArray(VAO1);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glEnableVertexAttribArray(0);
+    
+    // Al doilea triunghi
+    glGenBuffers(1, &VBO2);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
+    glGenVertexArrays(1, &VAO2);
+    glBindVertexArray(VAO2);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glEnableVertexAttribArray(0);
+    
+    glBindVertexArray(0);
+}
+
+
+
+bool initOpenGLWindow()
+{
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not start GLFW3\n");
+        return false;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //window scaling for HiDPI displays
+    glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
+
+    // for multisampling/antialising
+    glfwWindowHint(GLFW_SAMPLES, 4);
+
+    glWindow = glfwCreateWindow(glWindowWidth, glWindowHeight, "OpenGL Shader Example", NULL, NULL);
+    if (!glWindow) {
+        fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+        glfwTerminate();
+        return false;
+    }
+
+    glfwSetWindowSizeCallback(glWindow, windowResizeCallback);
+    glfwMakeContextCurrent(glWindow);
+
+#if not defined (__APPLE__)
+    // start GLEW extension handler
+    glewExperimental = GL_TRUE;
+    glewInit();
+#endif
+
+    // get version info
+    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+    const GLubyte* version = glGetString(GL_VERSION); // version as a string
+    printf("Renderer: %s\n", renderer);
+    printf("OpenGL version supported %s\n", version);
+
+    //for RETINA display
+    glfwGetFramebufferSize(glWindow, &retina_width, &retina_height);
+
+    return true;
+}
+
+void renderScene()
+{
+    //initializeaza buffer-ele de culoare si adancime inainte de a rasteriza cadrul curent
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //defineste culoarea de fundal
+    glClearColor(0.8, 0.8, 0.8, 1.0);
+    //specifica locatia si dimensiunea ferestrei
+    glViewport(0, 0, retina_width, retina_height);
+    //proceseaza evenimentele de la tastatura
+    if (glfwGetKey(glWindow, GLFW_KEY_A)) {
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO1);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+    if (glfwGetKey(glWindow, GLFW_KEY_D)) {
+        glUseProgram(shaderProgramGreen);
+        glBindVertexArray(VAO2);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+    //activeaza program shader-ul; apeluri ulterioare de rasterizare vor utiliza acest program
+    
+    glBindVertexArray(0);
+}
+
+std::string readShaderFile(std::string fileName)
+{
+    std::ifstream shaderFile;
+    std::string shaderString;
+
+    //open shader file
+    shaderFile.open(fileName);
+
+    std::stringstream shaderStringStream;
+
+    //read shader content into stream
+    shaderStringStream << shaderFile.rdbuf();
+
+    //close shader file
+    shaderFile.close();
+
+    //convert stream into GLchar array
+    shaderString = shaderStringStream.str();
+    return shaderString;
+}
+
+void shaderCompileLog(GLuint shaderId)
+{
+    GLint success;
+    GLchar infoLog[512];
+
+    //check compilation info
+    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shaderId, 512, NULL, infoLog);
+        std::cout << "Shader compilation error\n" << infoLog << std::endl;
+    }
+}
+
+void shaderLinkLog(GLuint shaderProgramId)
+{
+    GLint success;
+    GLchar infoLog[512];
+
+    //check linking info
+    glGetProgramiv(shaderProgramId, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "Shader linking error\n" << infoLog << std::endl;
+    }
+}
+
+GLuint initBasicShader(std::string vertexShaderFileName, std::string fragmentShaderFileName)
+{
+    GLuint shaderProgram;
+
+    //read, parse and compile the vertex shader
+    std::string v = readShaderFile(vertexShaderFileName);
+    const GLchar* vertexShaderString = v.c_str();
+    GLuint vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderString, NULL);
+    glCompileShader(vertexShader);
+    //check compilation status
+    shaderCompileLog(vertexShader);
+
+    //read, parse and compile the fragment shader
+    std::string f = readShaderFile(fragmentShaderFileName);
+    const GLchar* fragmentShaderString = f.c_str();
+    GLuint fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderString, NULL);
+    glCompileShader(fragmentShader);
+    //check compilation status
+    shaderCompileLog(fragmentShader);
+
+    //attach and link the shader programs
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    //check linking info
+    shaderLinkLog(shaderProgram);
+
+    return shaderProgram;
+}
+
+void cleanup() {
+    glfwDestroyWindow(glWindow);
+    //close GL context and any other GLFW resources
+    glfwTerminate();
+}
+
+int main(int argc, const char * argv[]) {
+
+    if (!initOpenGLWindow()) {
+        glfwTerminate();
+        return 1;
+    }
+
+    initObjects();
+
+    shaderProgram = initBasicShader("shaders/shader.vert", "shaders/shader.frag");
+    shaderProgramGreen = initBasicShader("shaders/shader_green.vert", "shaders/shader_green.frag");
+
+    while (!glfwWindowShouldClose(glWindow)) {
+        renderScene();
+
+        glfwPollEvents();
+        glfwSwapBuffers(glWindow);
+    }
+
+    cleanup();
+
+    return 0;
+}
---
+++ Lab4/shaders/shaderStart.vert
@@ -0,0 +1,11 @@
+#version 410 core
+
+layout(location = 0) in vec3 vertexPosition;
+layout(location = 1) in vec3 vertexColour;
+
+out vec3 colour;
+
+void main() {
+	colour = vertexColour;
+	gl_Position = vec4(vertexPosition, 1.0);
+}
